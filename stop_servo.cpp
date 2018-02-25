f49af72af720882bfe253feaaddc11045030f392
ConnorB123 <connor.j.byrnes@gmail.com>
2018-02-25 15:50:23 -0500

    added stop_servo

---
+++ stop_servo.cpp
@@ -0,0 +1,14 @@
+#include "PCA9685.h"
+#include <iostream>
+#include <stdio.h>
+//#include <ncurses.h>
+//#include <thread>
+#include <unistd.h>
+
+using namespace std;
+int main () {
+	pwm1.setPWM(0,0,0);
+	pwm2.setPWM(1,0,0);
+
+	return 0;
+}
